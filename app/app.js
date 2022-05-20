be1a471bf58c3c1954479c6950e031e1faaa3482
DESKTOP-INMFNVM\gaura <gaurangbhatt19@gmail.com>
2022-05-20 11:22:46 +0530

    UI changes and Date

--- app/app.js
+++ app/app.js
@@ -1,19 +1,44 @@
 var userModule= angular.module("userModule",['ngRoute'])
 
 userModule.config(['$routeProvider',function($routeProvider){
-
+    
     $routeProvider
-    .when("/angular-js-playlist/index.html#!",{
-        templateUrl: "views/form.html"
+    .when("/",{
+        templateUrl: "app/views/form.html"
+        ,controller: "userModuleController"
     })
     .when("/details",{
-        templateUrl :"views/userDetails.html"
+        templateUrl :"app/views/useDetails.html",
+        controller:"userDetailsController"
+    })
+    .otherwise({
+        redirectTo:"/"
     })
 }])
 
 
-userModule.controller("userModuleController",function($scope,$location){
-    console.log($scope.url)
- $location.path("/")
-})
+userModule.controller("userModuleController",["$scope","$location","$rootScope",function($scope,$location,$rootScope){
+    $scope.getUserDetails=function(user){
+        $location.path("/details")
+        let date=new Date(user.dateofjoining)
+        console.log(date.toLocaleDateString())
+        user.dateofjoining=date.toLocaleDateString().toString()
+        console.group(typeof user.dateofjoining)
+        $rootScope.$broadcast("Formsubmitted",user)
+        console.log(user)
+    }
+}])
 
+userModule.controller("userDetailsController",["$scope","$rootScope",function($scope,$rootScope){
+   $rootScope.$on("Formsubmitted",function(evnt,data){
+       console.log(data)
+       let res=[]
+       for(let [key,value] of Object.entries(data)){
+           res.push(value)
+       }
+       $scope.details=res
+       console.log($scope.details)
+   })
+
+   
+}])
